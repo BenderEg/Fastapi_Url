86c1da0bb4f324a107b6f4165610bfb7616aa052
egor nikitin <eanikitin90gmail.com>
2023-10-30 21:40:39 +0400

    clean unnecessary imports

--- app/src/db/redis.py
+++ app/src/db/redis.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from redis.asyncio import Redis
 
-redis_conn: Redis = None
+redis_conn: Optional[Redis] = None
 
 async def get_redis() -> Redis:
     return redis_conn
--- app/src/models/url.py
+++ app/src/models/url.py
@@ -1,19 +1,6 @@
-from typing import Optional
-
 from pydantic import BaseModel, AnyUrl
 
 
 class UrlIn(BaseModel):
 
     incoming_link: AnyUrl
-
-class UrlOut(BaseModel):
-
-    scheme: str
-    host: str
-    username: Optional[str] = None
-    password: Optional[str] = None
-    port: Optional[int] = None
-    path: Optional[str] = None
-    query: Optional[str] = None
-    fragment: Optional[str] = None
